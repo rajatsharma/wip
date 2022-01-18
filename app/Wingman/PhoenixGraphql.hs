4ee4cf4e9a0694e287ffb576bb17d801af3619d6
Rajat Sharma <lunasunkaiser@gmail.com>
2022-01-18 16:46:28 +0530

    Add absinthe to phoenix project

--- app/Wingman/PhoenixGraphql.hs
+++ app/Wingman/PhoenixGraphql.hs
@@ -1,8 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Wingman.PhoenixGraphql where
 
-import System.Directory (getCurrentDirectory)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import System.Directory (getCurrentDirectory, setCurrentDirectory)
 import System.Process (callCommand, callProcess)
 import Text.Casing (fromWords, toQuietSnake)
+import Prelude hiding ((<>))
+
+replaceStrInFile :: FilePath -> T.Text -> T.Text -> IO ()
+replaceStrInFile fileName needle replacement = T.readFile fileName >>= \txt -> T.writeFile fileName (T.replace needle replacement txt)
+
+replaceMarker :: T.Text
+replaceMarker = "  defp deps do\n    ["
+
+(<>) :: T.Text -> T.Text -> T.Text
+(<>) = T.append
 
 run :: String -> IO ()
 run name = do
@@ -10,7 +24,11 @@ run name = do
   _ <- callCommand $ "echo 'y' | mix phx.new " ++ name ++ " --no-html --no-assets --no-gettext --no-mailer"
   print "Welcome again, mix is done, I'll take it from here"
   cwd <- getCurrentDirectory
-  let projectDir = cwd ++ "/" ++ name ++ "/"
-  _ <- callProcess "rm" ["-rf", projectDir ++ "test"]
-  _ <- callProcess "rm" ["-rf", projectDir ++ "lib/" ++ toQuietSnake (fromWords name) ++ "/controllers/"]
+  setCurrentDirectory $ cwd ++ "/" ++ name
+  callCommand "rm -rf test"
+  callCommand $ "rm -rf lib/" ++ name ++ "_web/controllers/"
+  replaceStrInFile "mix.exs" replaceMarker $ replaceMarker <> T.pack "{:absinthe, \"~> 1.6\"},{:absinthe_plug, \"~> 1.5\"},{:absinthe_gen, \"~> 0.2\"},{:credo, \"~> 1.6\", only: [:dev, :test], runtime: false},"
+  callCommand "mix deps.get"
+  callCommand "mix format"
+  callCommand "mix credo gen.config"
   pure ()
