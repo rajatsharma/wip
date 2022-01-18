696477f92e52e1d8c31af6b5fc17ba934695d7ba
Rajat Sharma <lunasunkaiser@gmail.com>
2022-01-18 16:06:47 +0530

    Improve phoenix-graphql generation

--- app/Wingman/PhoenixGraphql.hs
+++ app/Wingman/PhoenixGraphql.hs
@@ -1,14 +1,16 @@
 module Wingman.PhoenixGraphql where
 
 import System.Directory (getCurrentDirectory)
-import System.Process (callProcess)
+import System.Process (callCommand, callProcess)
 import Text.Casing (fromWords, toQuietSnake)
 
 run :: String -> IO ()
 run name = do
-  _ <- callProcess "mix" ["phx.new", name, "--no-html", "--no-assets", "--no-gettext", "--no-mailer"]
+  print "Welcome to Wingman, I'll transfer this task to mix because it knows better"
+  _ <- callCommand $ "echo 'y' | mix phx.new " ++ name ++ " --no-html --no-assets --no-gettext --no-mailer"
+  print "Welcome again, mix is done, I'll take it from here"
   cwd <- getCurrentDirectory
-  let projectDir = cwd ++ "/" ++ name
-  _ <- callProcess "rm" ["-rf", "test"]
-  _ <- callProcess "rm" ["-rf", "lib/" ++ toQuietSnake (fromWords name) ++ "/controllers/"]
+  let projectDir = cwd ++ "/" ++ name ++ "/"
+  _ <- callProcess "rm" ["-rf", projectDir ++ "test"]
+  _ <- callProcess "rm" ["-rf", projectDir ++ "lib/" ++ toQuietSnake (fromWords name) ++ "/controllers/"]
   pure ()
